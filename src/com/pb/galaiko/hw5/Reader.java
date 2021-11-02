0801d22b2b1f0d08d720449fd68dd3e4102ec542
Vladimir Galaiko <dn310783gva1@gmail.com>
2021-11-02 12:41:16 +0200

    com2

--- src/com/pb/galaiko/hw5/Reader.java
+++ src/com/pb/galaiko/hw5/Reader.java
@@ -5,7 +5,7 @@ public class Reader {
 
     String fio;
     int numchitbilet;
-    String facuktet;
+    String facuklet;
     String birthday;
     String telefon;
     int kolknig;
@@ -14,21 +14,21 @@ public class Reader {
     public Reader(String fio, int numchitbilet, String facultet, String birthday, String telefon) {
         this.fio = fio;
         this.numchitbilet = numchitbilet;
-        this.facuktet = facultet;
+        this.facuklet = facultet;
         this.birthday = birthday;
         this.telefon = telefon;
     }
 
 
     public void takeBook(int kolknig) {
-        System.out.println(fio + " Взял книги, колличество : " + kolknig);
+        System.out.println(fio +"." + " Взял книги, колличество : " + kolknig);
     }
 
     public void takeBook(Book... takedBooks) {
-        System.out.println(fio + " Взял книги : " + takedBooks[0].name +
+        System.out.println(fio + "." + " Взял книги : " + takedBooks[0].name +
                 ", " + takedBooks[1].name + ", " + takedBooks[2].name);
 
-        System.out.println(fio + " Взял книги : ");
+        System.out.println(fio + "." + " Взял книги : ");
         for (int i = 0; i < 3; i++) {
             System.out.println(takedBooks[i].name + "(" + takedBooks[i].avtor + ", "
                     + takedBooks[i].god + ").");
@@ -36,14 +36,14 @@ public class Reader {
     }
 
     public void returnBook(int kolknig) {
-        System.out.println(fio + " Вернул книги, колличество : " + kolknig);
+        System.out.println(fio +"." + " Вернул книги, колличество : " + kolknig);
     }
 
     public void returnBook(Book... returnBook) {
-        System.out.println(fio + " Вернул книги : " + returnBook[0].name +
+        System.out.println(fio +"."+ " Вернул книги : " + returnBook[0].name +
                 ", " + returnBook[1].name + ", " + returnBook[2].name + ".");
 
-        System.out.println(fio + " Вернул книги : ");
+        System.out.println(fio +"." + " Вернул книги : ");
         for (int i = 0; i < 3; i++) {
             System.out.println(returnBook[i].name + "(" + returnBook[i].avtor +
                     ", " + returnBook[i].god + ").");
@@ -56,7 +56,7 @@ public class Reader {
         return "" +
                 "ФИО: " + fio +
                 ". Номер читательского билета: " + numchitbilet +
-                ". Факультет: " + facuktet +
+                ". Факультет: " + facuklet +
                 ". Дата рождения: " + birthday + ". Телефон "
                 + telefon + "";
     }
